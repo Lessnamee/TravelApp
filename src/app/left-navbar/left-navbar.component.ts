f81eddaa275e4dd37a36c0447936a150b5b77785
Kinga Spytkowska <kinguia319@interia.pl>
2023-09-12 19:51:21 +0200

    first commit

---
+++ src/app/left-navbar/left-navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'left-navbar',
+  templateUrl: './left-navbar.component.html',
+  styleUrls: ['./left-navbar.component.css']
+})
+export class LeftNavbarComponent {
+}
