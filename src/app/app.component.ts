3c37b2b92151a7247f17f5b42c2e79658b9b09b5
radzikkos <lukaszrad99@wp.pl>
2023-11-22 18:02:20 +0100

    add magic animation on routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { HeaderComponent } from './Components/Navi/header/header.component';
 import { Observable } from 'rxjs';
+import { slideInAnimation } from './animations/route-animation';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [ slideInAnimation ]
 })
 export class AppComponent implements OnInit {
   title = 'travel app';
