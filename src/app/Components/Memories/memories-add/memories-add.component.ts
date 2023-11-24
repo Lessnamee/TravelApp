68fa4b454e242eec4cf55e5a4fed1f466acde45e
Kinga Spytkowska <kinguia319@interia.pl>
2023-11-24 14:02:10 +0100

    button in memories

--- src/app/Components/Memories/memories-add/memories-add.component.ts
+++ src/app/Components/Memories/memories-add/memories-add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router} from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
@@ -12,7 +13,8 @@ export class MemoriesAddComponent {
   constructor(
     private formBuilder: FormBuilder,
     private afs: AngularFirestore,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   memoryForm: FormGroup = this.formBuilder.group({
@@ -24,22 +26,31 @@ export class MemoriesAddComponent {
   addMemory() {
     if (this.memoryForm.valid) {
       const { name, description, date } = this.memoryForm.value;
-      if (name && date) {
+  
+      if (name && description && date) {
         const memoryData = {
           name: name,
           description: description,
           date: date,
           userId: this.authService.getLoggedInUser().uid 
         };
-
+  
         this.afs.collection('memories').add(memoryData).then(docRef => {
           console.log('Dodano nowy dokument z ID: ', docRef.id);
+          this.redirectToMemorySee(docRef.id);
         }).catch(error => {
           console.error('Błąd podczas dodawania dokumentu: ', error);
         });
 
         this.memoryForm.reset();
+      } else {
+        console.warn('Wypełnij wszystkie pola przed dodaniem pamięci.');
       }
     }
+  }
+  
+
+  redirectToMemorySee(memoryId: string) {
+     this.router.navigate(['/memory-see']);
   }
 }
