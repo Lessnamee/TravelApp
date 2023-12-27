9d4bd3c56cacfa5015aa10a69649d7b53ecf3fd3
Kinga Spytkowska <kinguia319@interia.pl>
2023-12-27 18:41:25 +0100

    poprawki

--- src/app/Components/Auth/register/register.component.ts
+++ src/app/Components/Auth/register/register.component.ts
@@ -1,3 +1,28 @@
+// import { Component } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { AuthService } from 'src/app/shared/services/auth.service';
+
+// @Component({
+//   selector: 'register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.css'],
+// })
+// export class RegisterComponent {
+
+//   constructor(
+//     private formBuilder: FormBuilder, public authService: AuthService
+//   ) {}
+
+//   signupForm: FormGroup = this.formBuilder.group({
+//     email: ['', [Validators.required, Validators.email]],
+//     passwords: this.formBuilder.group({
+//       password: ['', [Validators.required, Validators.minLength(8)]],
+//       repeatpassword: ['', [Validators.required, Validators.minLength(8)]],
+//     }),
+//   });
+
+// }
+
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -8,17 +33,30 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
   constructor(
-    private formBuilder: FormBuilder, public authService: AuthService
+    private formBuilder: FormBuilder,
+    public authService: AuthService
   ) {}
 
   signupForm: FormGroup = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
-    passwords: this.formBuilder.group({
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      repeatpassword: ['', [Validators.required, Validators.minLength(8)]],
-    }),
+    passwords: this.formBuilder.group(
+      {
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        repeatpassword: ['', [Validators.required, Validators.minLength(8)]],
+      },
+      { validator: this.passwordsMatchValidator }
+    ),
   });
 
+  passwordsMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const repeatPassword = formGroup.get('repeatpassword')?.value;
+
+    if (password !== repeatPassword) {
+      formGroup.get('repeatpassword')?.setErrors({ notSame: true });
+    } else {
+      formGroup.get('repeatpassword')?.setErrors(null);
+    }
+  }
 }
