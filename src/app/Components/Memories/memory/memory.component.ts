e8fe2a7571316a40549a98f617fc7740b26cf1a8
radzikkos <lukaszrad99@wp.pl>
2023-12-28 11:52:54 +0100

    more remove button to memory

--- src/app/Components/Memories/memory/memory.component.ts
+++ src/app/Components/Memories/memory/memory.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-memory',
@@ -8,5 +8,10 @@ import { Component, Input } from '@angular/core';
 export class MemoryComponent {
   @Input() name: string;
   @Input() description: string;
+  @Output() removeMemoryEvent = new EventEmitter<void>();
 
+  removeMemoryHandler(event: Event): void {
+    event.stopPropagation();
+    this.removeMemoryEvent.emit();
+  }
 }
